f63c02da6131eac82d81b4367d0ec253701c73b3
msantam2 <msantam2@gmail.com>
2016-11-18 20:58:44 -0800

    PeriodicTable component able to render a list of chemical element names in the browser - i.e. periodic-table API working

--- components/periodic_table.jsx
+++ components/periodic_table.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import pt from 'periodic-table';
 
 class PeriodicTable extends React.Component {
+
+
   render() {
+    let allElements = pt.all();
+    allElements = allElements.map(elem => {
+      return <li>{elem.name}</li>;
+    });
+
     return (
-      <div></div>
+      <div>
+        <ul>
+          {allElements}
+        </ul>
+      </div>
     );
   }
 }
